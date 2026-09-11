0b6330233454f937038bfe10b59fa6404a547d41
toky6661-hub <toky6661@gmail.com>
2026-09-11 17:51:44 +0800

    fix: 更新数据库配置及自动建表逻辑

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -7,9 +7,13 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
-  ssl: {                        // ← 这里！原来是 sll，改正为 ssl
-    rejectUnauthorized: true
-  },
+  
+  // ← 这里！原来是 sll，改正为 ssl
+  ssl: {
+  minVersion: "TLSv1.2",
+  rejectUnauthorized: true
+},
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
